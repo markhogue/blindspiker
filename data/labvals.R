cc95bd2f4c170a942c2ce1622a90554dc3cce3cc
Mark Hogue <mark.hogue.chp@gmail.com>
2026-04-20 15:26:38 -0400

    document example data sets

---
+++ data/labvals.R
@@ -0,0 +1,14 @@
+#' Example Laboratory Values
+#'
+#' @format ## `labvals`
+#' A data frame with 225 rows and 8 columns:
+#' \describe{
+#'   \item{sample_ID}{Unique sample identifier}
+#'   \item{analyte}{what is being analyzed}
+#'   \item{result_date}{date of analysis}
+#'   \item{k}{uncertainty coverage factor}
+#'   \item{result}{value of analysis}
+#'   \item{unc}{measurement uncertainty in result units}
+#'   \item{units}{units of measure}
+#'   \item{det_lvl}{minimum level of detection}
+#' }
---
+++ data/spikevals.R
@@ -0,0 +1,15 @@
+#' Example Spike Values
+#'
+#' @format ## `spikevals`
+#' A data frame with 225 rows and 8 columns:
+#' \describe{
+#'   \item{sample_ID}{Unique sample identifier}
+#'   \item{analyte}{what is being analyzed}
+#'   \item{spike_value}{measure of sample spiking}
+#'   \item{sv_unc}{uncertainty spike value}
+#'   \item{sv_k}{coverage factor of uncertainty}
+#'   \item{spike_unit}{unit of measure}
+#'   \item{units}{units of measure}
+#'   \item{provider_lab}{origin of spike sample}
+#'   \item{submission_date}{date provided to lab}
+#' }
